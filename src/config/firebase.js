55d37c1392797c97fa08990a75e59442b6b37290
sai teja <94728002+Saiteja6393@users.noreply.github.com>
2024-01-15 19:25:20 +0530

    Add files via upload

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from 'firebase/auth'
+import { Firestore, getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyAchOjZWtweJgH3UrZzGRzzkTZPsEjP5X8",
+  authDomain: "userauth-ea0f8.firebaseapp.com",
+  projectId: "userauth-ea0f8",
+  storageBucket: "userauth-ea0f8.appspot.com",
+  messagingSenderId: "228805725398",
+  appId: "1:228805725398:web:10ea429038262111090d77",
+  measurementId: "G-NQC00HXPKL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
+export {auth};
